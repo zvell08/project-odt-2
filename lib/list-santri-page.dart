c0f1dc43da6fc5e0410324981560843bd1db2c0c
shahnaz hr <shahnazhasyar@gmail.com>
2023-01-28 13:43:13 +0700

    ini list santri

---
+++ lib/list-santri-page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+
+
+class listSantri extends StatelessWidget {
+  final List santri = [
+    "Royyan",
+    "Marvel",
+    "Haritsah",
+    "Izzul",
+    "Izam",
+    "fariska",
+    "zaki",
+    "azka",
+    "aufa",
+    "fatih",
+    "furqan",
+    "ali"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.cyan[300],
+          title: Text("Santri"),
+        ),
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            return Card(
+              child: ListTile(
+                  title: Text(
+                      santri[index],
+                      style: TextStyle(fontSize: 30)
+                  ),
+                  subtitle: Text('Halaqoh Ustadz Ismail'),
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.cyan[100],
+                    child: Text(
+                        santri[index][0], // ambil karakter pertama text
+                        style: TextStyle(
+                            color: Color(0xFF4DD0E1),
+                            fontSize: 20)
+                    ),
+                  )
+              ),
+            );
+          },
+          itemCount: santri.length,
+        )
+    );
+  }
+}
